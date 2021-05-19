8e540890890a4b77c27bdd9e52f6cc912ed35cc1
cohessel <larissahessel@outlook.com>
2021-05-18 23:48:08 -0300

    agora é possível captar a input de caracter do usuário :)

--- ex_1_anagramas/src/Input.java
+++ ex_1_anagramas/src/Input.java
@@ -1,2 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Input {
+    public static void main(String[] args) {
+
+        Scanner inputUsuario = new Scanner(System.in);
+
+        String palavra;
+
+        do {
+            try {
+                System.out.println("> Digite um anagrama...");
+                palavra = inputUsuario.next();
+            } catch (Exception erro){
+                System.out.println("Ixi, algo deu errado :/");
+            }
+        } while (inputUsuario == null);{
+            System.out.println("--------------------------------");
+        }
+    }
 }
